3ec05f9f7e7e74b23193703318f4edbbb43d5439
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 14:34:42 -0800

    Print string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin python3
+def uppercase(str):
+    length = len(str)
+    for i in range(length):
+        if ord(str[i]) <= 122 and ord(str[i]) >= 97:
+            #CONVERT TO UPPER BY -32
+            ch_r = chr(ord(str[i])-32)
+        else:
+            ch_r = str[i]
+
+        if i != length - 1:
+            print("{}".format(ch_r), end="")
+        else:
+            print("{}".format(ch_r))
