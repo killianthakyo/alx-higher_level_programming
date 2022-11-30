a8d8e9df685b8b6286f7d8f9fedea1deceb6a297
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 14:44:07 -0800

    Power of a number

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
-    product = 1;
+    product = 1
     for i in range(abs(b)):
         product = product * a
     if b < 0:
         product = 1 / product
     return product
-
