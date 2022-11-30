64184a744a354c95ee5cc918c8fd736297cc8173
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 10:05:27 -0800

    prints the ASCII alphabet, in lowercase, not followed by a new line except q and e

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # CODE THATPRINTS THE ASCII alphabet, in lowercase
 for letter in range(97, 123):
-    #print("chr({}),end=''".format(letter))
     print("{}".format(chr(letter)), end="")
