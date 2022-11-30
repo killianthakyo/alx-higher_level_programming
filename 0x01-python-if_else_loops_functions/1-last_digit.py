41697fb6cb91fe86ca9c75aad329d314064a098b
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 05:21:59 -0800

    Last digit of an int

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,7 @@ number = random.randint(-10000, 10000)
 num_str = repr(number)
 last_digit_str = num_str[-1]
 last_digit = int(last_digit_str)
-signed = ""
-if number < 0:
+if number < 0 and last_digit != 0:
     last_digit = -abs(last_digit)
 if last_digit > 5:
     status = "and is greater than 5"
@@ -14,5 +13,4 @@ elif last_digit == 0:
     status = "and is 0"
 else:
     status = "and is less than 6 and not 0"
-
 print("Last digit of {} is {} {}".format(number, last_digit, status))
