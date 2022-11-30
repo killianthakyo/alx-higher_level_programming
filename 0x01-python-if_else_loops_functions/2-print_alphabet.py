cf1285d525129a3a968cac3e9d085f01652a023b
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 09:53:27 -0800

    Print the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # CODE THATPRINTS THE ASCII alphabet, in lowercase
 for letter in range(97, 123):
-    print(chr(letter),end="")
+    #print("chr({}),end=''".format(letter))
+    print("{}".format(chr(letter)), end="")
