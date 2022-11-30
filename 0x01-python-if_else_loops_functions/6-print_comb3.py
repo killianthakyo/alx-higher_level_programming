318aeef24b868381909d010a22156933d5ecc06b
Killian Nthakyo <killynthakyo@gmail.com>
2022-11-30 14:27:29 -0800

    Print comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def determine(num):
+    num = str(num)
+    if len(num) < 2:
+        num = "0" + num
+    reversed_num = num[::-1]
+    if num < reversed_num:
+        return True
+    else:
+        return False
+
+
+first = True
+for i in range(0, 100):
+    #determine(i)
+    if determine(i):
+        if first:
+            print("{:0>2d}".format(i), end="")
+            first = False
+        else:
+            print(", {:0>2d}".format(i), end="")
