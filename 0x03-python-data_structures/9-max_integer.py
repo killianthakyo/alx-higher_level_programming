7eeaa0eb30c98083c1cb1c0ff97507f00d8856c0
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-05 12:43:22 -0800

    Return max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max_value = None
     if len(my_list) > 0:
-        max_value = my_list[0];
+        max_value = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > max_value:
             max_value = my_list[i]
