db1c0d40c1bd2f8a23b69fb5131454c1ca919a9b
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-05 03:56:59 -0800

    Print integers in a list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,3 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     for intg in reversed(my_list):
         print("{:d}".format(intg))
+
+
+if __name__ == "__main__":
+    print_reversed_list_integer()
