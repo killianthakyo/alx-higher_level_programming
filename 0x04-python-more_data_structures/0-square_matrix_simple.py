d4df8e7778e55ff4c9c6198d3058b7888e5ec86e
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-07 11:44:45 -0800

    Square of matrix with lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-        R = list(map(lambda x: x**2,i))
+        R = list(map(lambda x: x**2, i))
         new_matrix.append(R)
     return new_matrix
-
