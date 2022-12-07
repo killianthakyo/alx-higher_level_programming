8d50f062426241ba3215c3ea138ca0a95d12af38
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-07 13:51:35 -0800

    Return a new dictionary with all values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    a_dictionary = {}
+    if a_dictionary:
+        for key, val in a_dictionary.items():
+            new_dict.update({key: val * 2})
+        return (new_dict)
