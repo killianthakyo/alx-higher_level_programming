fffb1a623fb3e595bce20f976117896f987e2d63
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-20 15:43:00 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define an empty class called Square"""
+
+
+class Square:
+    """Initialize the Square class"""
+    def __init__(self, size):
+        self.__size = size
+
