ca3d2224a797856a5f0253d8f2a2a3319a48ab20
Killian Nthakyo <killynthakyo@gmail.com>
2022-12-21 11:25:19 -0800

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size**2
