858890ae740102ac219f51526eb444f7cafb45e4
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-04 13:00:56 -0800

    Magic class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-LockedClass
-"""
+"""Defines a locked class."""
 
 
 class LockedClass:
-    """ No class or object attributes, can't set
-        Except for first_name
     """
-    def __setattr__(self, attribute, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+    Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
+    """
+
+    __slots__ = ["first_name"]
