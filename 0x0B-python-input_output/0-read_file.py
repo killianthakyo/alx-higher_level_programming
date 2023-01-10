4013daf9571aeb6ad9cc485d6c9f9a73062fb9d9
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-10 07:11:31 -0800

    Python Input/Output - Read a textfile content and print in stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-"""Write into a file"""
+"""Read a file"""
 
 
 def read_file(filename=""):
+    """
+    Read a text (UTF8) AND print it in stdout
+    """
     with open(filename, mode="r", encoding="utf-8") as myFile:
         print(myFile.read())
