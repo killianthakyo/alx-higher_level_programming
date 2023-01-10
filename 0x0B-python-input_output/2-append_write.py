a2f695da46c1358ab6606abd36533a50b71912b7
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-10 09:48:38 -0800

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """
+    Append and return number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return(f.write(txt))
