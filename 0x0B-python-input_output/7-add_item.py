34abbde88b4671d6bc393156768d39e47bb287df
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-10 11:59:13 -0800

    Add all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Add all arguments to a Python list, then save them to a file
+"""
+import sys, json
+
+filename = "add_item.json"
+myArgs = sys.argv
+myArgs = myArgs[1:]
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file(myArgs, filename)
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+my_list = load_from_json_file(filename)
