c4eb78d35d0040b1708b7865f37763fd014a520a
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-11 18:58:12 -0800

    Raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Class with an instance method
+'''
+
+
+class BaseGeometry:
+    '''
+    Empty class
+    '''
+    def area(self):
+        '''
+        Throw exception
+        '''
+        raise Exception("area() is not implemented")
