198dd049aef78137c6240b636187de04a50a6292
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-11 18:27:49 -0800

    Determine if object is an instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,4 +3,6 @@ def is_same_class(obj, a_class):
     '''
     Determine if an object is exactly an instance of specified class
     '''
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return True
+    return False
