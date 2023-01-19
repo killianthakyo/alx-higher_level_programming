2262e2d827d15af20351061bfa2769f3faf196b8
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-19 00:34:57 -0800

    Create a rectangle class that inherit from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 '''
 Rectangle class that inherits frome base class
 '''
+from models.base import Base
 
 
 class Rectangle (Base):
@@ -15,36 +16,44 @@ class Rectangle (Base):
         self.__height = height
         self.__x = x
         self.__y = y
-        Base.__init(id)
+        Base.__init__(id)
 
     @property
-    def get_width(self):
+    def width(self):
+        '''Get width of a rectangle'''
         return self.__width
 
     @width.setter
-    def set_width(self, value):
+    def width(self, value):
+        '''set the width of a rectangle'''
         self.__width = value
 
     @property
-    def get_height(self):
+    def height(self):
+        '''set the height of a rectangle'''
         return self.__height
 
     @height.setter
-    def set_height(self, value):
+    def height(self, value):
+        '''set the height of a rectangle'''
         self.__height = value
 
     @property
-    def get_x(self):
+    def x(self):
+        '''set the x-cordinate'''
         return self.__x
 
     @x.setter
-    def set_x(self, value):
+    def x(self, value):
+        '''set the x-coordinate'''
         self.__x = value
 
     @property
-    def get_y(self):
+    def y(self):
+        '''get the y coordinate'''
         return self.__y
 
     @y.setter
-    def set_y(self, value):
+    def y(self, value):
+        '''set the y coordinate'''
         self.__y = value
