3309ee4ec6daa95e4a325475e999f16f7390d950
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-19 00:41:36 -0800

    Create a rectangle class that inherit from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,10 @@ class Rectangle (Base):
     @width.setter
     def width(self, value):
         '''set the width of a rectangle'''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -36,6 +40,10 @@ class Rectangle (Base):
     @height.setter
     def height(self, value):
         '''set the height of a rectangle'''
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -46,6 +54,10 @@ class Rectangle (Base):
     @x.setter
     def x(self, value):
         '''set the x-coordinate'''
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -56,4 +68,8 @@ class Rectangle (Base):
     @y.setter
     def y(self, value):
         '''set the y coordinate'''
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value <= 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
