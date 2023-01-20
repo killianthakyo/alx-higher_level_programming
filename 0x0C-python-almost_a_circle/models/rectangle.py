44b5988f2b70dd4bcaccc16b5f6cdffca7afa7c8
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-20 14:59:42 -0800

    *kwags

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,46 @@ class Rectangle (Base):
         '''Override the __str__ method to return desired format'''
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle instance with args
+        Arguments:
+            *args:
+                arg1 (int): 1st argument which is id
+                arg2 (int): 2nd arg which is width
+                arg3 (int): 3rd arg which is height
+                arg4 (int): 4th arg which is x
+                arg5 (int): 5th arg which is y
+        """
+        if (args) and (len(args) != 0):
+            count = 0
+            for arg in args:
+                if count == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif count == 1:
+                    self.width = arg
+                elif count == 2:
+                    self.height = arg
+                elif count == 3:
+                    self.x = arg
+                elif count == 4:
+                    self.y = arg
+                count += 1
+        elif (kwargs) and (len(kwargs) != 0):
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
