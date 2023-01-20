d300086e4a87bf74c34b48e2668801ac1b94c40a
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-20 14:27:33 -0800

    Override __str__ method to return desired output

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,4 +87,4 @@ class Rectangle (Base):
 
     def __str__(self):
         '''Override the __str__ method to return desired format'''
-        return f'[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}'
+        return f'[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}'
