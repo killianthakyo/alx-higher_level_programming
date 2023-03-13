dca642d450b4ea096988c597d70d5dee4d6c4678
Killian Nthakyo <killynthakyo@gmail.com>
2023-03-13 15:32:12 -0700

    Value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const myVar = process.argv.length;
-if (myVar === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
