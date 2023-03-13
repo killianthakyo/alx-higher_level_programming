e2ad3cfd99c12e1aaa4803b867103e297d176dfc
Killian Nthakyo <killynthakyo@gmail.com>
2023-03-13 16:17:24 -0700

    Check if variable can be converted to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
-if (isNaN(myVar)) {
-  console.log('Not a number');
-} else {
+if (Number.isInteger(parseInt(myVar))) {
   console.log(`My number: ${parseInt(myVar)}`);
+} else {
+  console.log('Not a number');
 }
