b54054acf55824de8d03f0098b342c14db63a5a1
Killian Nthakyo <killynthakyo@gmail.com>
2023-03-20 14:36:58 -0700

    Python Object Relational Mapping. Fetch states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 """List all states from the database"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv
+            [1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
