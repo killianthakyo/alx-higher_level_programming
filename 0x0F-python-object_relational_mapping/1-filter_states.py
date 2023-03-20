acefd138d044eff8db8e1684a2e02951298322a1
Killian Nthakyo <killynthakyo@gmail.com>
2023-03-20 15:13:31 -0700

    Python Object Relational Mapping. Fetch states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" lists all states from the database"""
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+"""
+Script to list all states from a DB with name starting
+with 'N'
+"""
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+    cur.execute("SELECT * FROM `states` order by `id`")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
