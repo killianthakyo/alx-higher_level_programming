0c7591a351d5f1ae350c0571a2bdee20dfd8c6cc
Killian Nthakyo <killynthakyo@gmail.com>
2023-03-24 12:25:55 -0700

    Avoid SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name= %s ORDER BY id ASC",
-                (sys.argv[4],))
+    cur.execute(
+            "SELECT * FROM states WHERE name= %s ORDER BY id ASC", sys.argv[4])
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == sys.argv[4]:
