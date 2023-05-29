49ebf41510b09e4caf8897be4631a85e4b716712
killianthakyo <111245832+killianthakyo@users.noreply.github.com>
2023-05-29 09:15:32 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/* returns the reversed version of a list */
-exports.esrever = function (list) {
-  const rev = [];
-  for (const i in list) {
-    rev.unshift(list[i]);
-  }
-  return (rev);
-};
