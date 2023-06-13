efe0bf2c53a49e69d3b818fdb17e933262fc9959
Killian Nthakyo <killynthakyo@gmail.com>
2023-06-13 15:47:49 -0700

    Print a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++){
+        rect = rect + 'X';
+      }
+      console.log(rect);
+    }
+  }
+};
