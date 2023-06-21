c32c7d02a2aff7a7f6d32d6a39264a553fd88ef5
Killian Nthakyo <killynthakyo@gmail.com>
2023-06-21 12:01:34 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays entries
+in a db where the name matches the arg
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+                id ASC".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
+    cur.close()
+    db.close()
