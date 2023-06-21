18ab1b8ac06685526f6cb23d4ea769ada6045e48
Killian Nthakyo <killynthakyo@gmail.com>
2023-06-21 04:11:07 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
