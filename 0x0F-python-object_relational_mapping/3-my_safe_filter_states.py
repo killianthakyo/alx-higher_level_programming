78290c1d15e600a47aaab3c8f3fb7de8952bcb2b
Killian Nthakyo <killynthakyo@gmail.com>
2023-06-23 13:47:29 -0700

    SQL filter safe from injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
-                id ASC", (sys.argv[4]))
+                id ASC", (sys.argv[4],))
     [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
     cur.close()
     db.close()
