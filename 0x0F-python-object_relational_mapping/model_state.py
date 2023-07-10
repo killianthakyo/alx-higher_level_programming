bc8cdb1a98ed158dd0eb341ff93f08c1b217e8e1
Killian Nthakyo <killynthakyo@gmail.com>
2023-07-10 06:25:17 -0700

    State Model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Python class to create a model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Create states model
+    """
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
